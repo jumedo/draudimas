e3a0c950a7f2cfb50b21b6625bad60777fc70f1f
“julita <julita.medolinskaite@gmail.com>
2020-04-22 23:04:55 +0300

    First commit

---
+++ database/migrations/2020_04_22_184438_create_cars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('reg_number',20);
+            $table->string('brand', 60);
+            $table->string('model', 60)->nullable()->default(null);
+            $table->foreignId('owner_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
---
+++ database/migrations/2020_04_22_184943_create_owners_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOwnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('owners', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',60);
+            $table->string('surname',60);
+            $table->date('from_date' );
+            $table->date('to_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('owners');
+    }
+}
