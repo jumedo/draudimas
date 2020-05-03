0b65f565cfd069b381ea28907eb7849d32e691b2
“julita <julita.medolinskaite@gmail.com>
2020-05-03 15:50:47 +0300

    setLocale

---
+++ resources/lang/en/cars.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'reg_number' => 'reg_number',
+    'brand' => 'brand',
+    'model' => 'model',
+    'owner_id'=>'owner_id',
+    'edit'=>'Edit',
+    'delete'=>'Delete',
+    'add_new'=>'Add new',
+];
+
+
---
+++ resources/lang/lt/cars.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'reg_number' => 'Registration number',
+    'brand' => 'Brand',
+    'model' => 'Model',
+    'owner_id'=>'Owner',
+    'edit'=>'Edit',
+    'delete'=>'Delete',
+    'add_new'=>'Add new',
+];
+
+
